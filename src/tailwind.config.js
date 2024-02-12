253bd7d53320ae6d90e4d44af75a495e7d8cc2b4
calthewebdev <sudofrancaalmeida@gmail.com>
2024-02-12 21:30:07 +0200

    h

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html'],
+  content: ['/home/webdevcal/repos/contact-form/build/*.html'],
   theme: {
     extend: {},
   },
